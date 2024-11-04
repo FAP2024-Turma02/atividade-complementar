9f2a1421dbbfaa57300b85f8b3057fa6dac113fa
AlansidneyJunior <alansidney.silva21@gmai.com>
2024-11-03 22:51:43 -0300

    Criando e testando Has_and_belongs_to_many association.

---
+++ db/migrate/20241104014106_create_assemblies_parts_join_table.rb
@@ -0,0 +1,8 @@
+class CreateAssembliesPartsJoinTable < ActiveRecord::Migration[7.0]
+  def change
+    create_table :assemblies_parts, id: false do |t|
+      t.belongs_to :assembly, null: false, foreign_key: true
+      t.belongs_to :part, null: false, foreign_key: true
+    end
+  end
+end
