047e957afa4b03b1c9ff493156531bf1adb2a897
AlansidneyJunior <alansidney.silva21@gmai.com>
2024-11-05 00:54:07 -0300

    Criando e Testando rotas Index e Show para Modelos Supplier e Account.

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,12 @@
+class AccountsController < ApplicationController
+
+    def index
+        accounts = Account.all
+        render json: accounts
+    end
+
+    def show
+        @account = Account.find(params[:id])
+        render json: @account
+    end
+end
---
+++ app/controllers/suppliers_controller.rb
@@ -0,0 +1,12 @@
+class SuppliersController < ApplicationController
+
+    def index
+        suppliers = Supplier.all
+        render json: suppliers
+    end
+
+    def show
+        @supplier = Supplier.find(params[:id])
+        render json: @supplier
+    end
+end
